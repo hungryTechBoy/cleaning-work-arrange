0674c71d103979767670f1bb83e136936da9e2bc
Bo Deng <bo.deng@shopee.com>
2019-10-22 16:32:38 +0800

    change schedule

--- proj/celery.py
+++ proj/celery.py
@@ -23,7 +23,7 @@ app.conf.update(
         'shovel_shit': {
             'task': 'tasks.send_shovel_shit_task',
             # 'schedule': crontab(minute=0, hour='21,22,23'),
-            'schedule': crontab(minute='17,18,19,20,21,22,23,24,25'),
+            'schedule': crontab(minute='1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59'),
             # 'schedule': timedelta(seconds=10),
 
         },
